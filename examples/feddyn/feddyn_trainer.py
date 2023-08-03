d0db5f9bd239a662579f6aa98f596e8a460dea3b
Tanushanth <79293445+Tanushanth@users.noreply.github.com>
2023-08-03 19:59:32 -0400

    Added custom train_model function with adaptive alpha coef

--- examples/feddyn/feddyn_trainer.py
+++ examples/feddyn/feddyn_trainer.py
@@ -3,10 +3,17 @@ import torch
 from plato.config import Config
 from plato.trainers import basic
 
-
+import copy
+import logging
+import multiprocessing as mp
+import os
+import pickle
+import re
+import time
+import numpy as np
 class Trainer(basic.Trainer):
 
-    def perform_forward_and_backward_passes(self, config, examples, labels):
+    def perform_forward_and_backward_passes(self, config, examples, labels, alpha_coef):
         """Perform forward and backward passes in the training loop.
 
         Arguments:
@@ -31,8 +38,6 @@ class Trainer(basic.Trainer):
         # self.optimizer.step()
 
         # return loss
-
-        alpha_coef = config["parameters"]["alpha_coef"]
         avg_mdl_param = config["parameters"]["avg_mdl_param"]
         local_grad_vector = config["parameters"]["local_grad_vector"]
 
@@ -67,3 +72,92 @@ class Trainer(basic.Trainer):
         self.optimizer.step()
         
         return loss
+    
+
+     def train_model(self, config, trainset, sampler, **kwargs):
+        """The default training loop when a custom training loop is not supplied."""
+        batch_size = config["batch_size"]
+        self.sampler = sampler
+        tic = time.perf_counter()
+
+        self.run_history.reset()
+
+        self.train_run_start(config)
+        self.callback_handler.call_event("on_train_run_start", self, config)
+
+        self.train_loader = self.get_train_loader(batch_size, trainset, sampler)
+
+        # Initializing the loss criterion
+        self._loss_criterion = self.get_loss_criterion()
+
+        # Initializing the optimizer
+        self.optimizer = self.get_optimizer(self.model)
+        self.lr_scheduler = self.get_lr_scheduler(config, self.optimizer)
+        self.optimizer = self._adjust_lr(config, self.lr_scheduler, self.optimizer)
+
+        self.model.to(self.device)
+        self.model.train()
+
+        total_epochs = config["epochs"]
+        n_clnt = config["total_clients"]
+        alpha_coef = config["alpha_coef"]
+
+        for self.current_epoch in range(1, total_epochs + 1):
+            self._loss_tracker.reset()
+            self.train_epoch_start(config)
+            self.callback_handler.call_event("on_train_epoch_start", self, config)
+
+            for batch_id, (examples, labels) in enumerate(self.train_loader):
+                self.train_step_start(config, batch=batch_id)
+                self.callback_handler.call_event(
+                    "on_train_step_start", self, config, batch=batch_id
+                )
+
+                examples, labels = examples.to(self.device), labels.to(self.device)
+
+                clnt_y = labels;
+                weight_list = np.asarray([len(clnt_y[i]) for i in range(n_clnt)])
+                weight_list = weight_list / np.sum(weight_list) * n_clnt
+                alpha_coef_adpt = alpha_coef / weight_list[self.client_id]
+
+                loss = self.perform_forward_and_backward_passes(
+                    config, examples, labels, alpha_coef_adpt
+                )
+
+                self.train_step_end(config, batch=batch_id, loss=loss)
+                self.callback_handler.call_event(
+                    "on_train_step_end", self, config, batch=batch_id, loss=loss
+                )
+
+            self.lr_scheduler_step()
+
+            if hasattr(self.optimizer, "params_state_update"):
+                self.optimizer.params_state_update()
+
+            # Simulate client's speed
+            if (
+                self.client_id != 0
+                and hasattr(Config().clients, "speed_simulation")
+                and Config().clients.speed_simulation
+            ):
+                self.simulate_sleep_time()
+
+            # Saving the model at the end of this epoch to a file so that
+            # it can later be retrieved to respond to server requests
+            # in asynchronous mode when the wall clock time is simulated
+            if (
+                hasattr(Config().server, "request_update")
+                and Config().server.request_update
+            ):
+                self.model.cpu()
+                model_path = Config().params["model_path"] 
+                filename = f"{model_path}_{self.client_id}.pth"
+                self.save_model(filename)
+                self.model.to(self.device)
+
+            self.run_history.update_metric("train_loss", self._loss_tracker.average)
+            self.train_epoch_end(config)
+            self.callback_handler.call_event("on_train_epoch_end", self, config)
+
+        self.train_run_end(config)
+        self.callback_handler.call_event("on_train_run_end", self, config)
